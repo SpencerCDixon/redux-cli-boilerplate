d5e8dd5f5ebd4f7d7f84760c239eab9655309b9b
Spencer Dixon <spencercdixon@gmail.com>
2016-07-06 14:08:49 -0400

    Changes view blueprint to use the word 'View' for clarity.  Adds some useful comments explaining the use of global css variables

---
+++ blueprints/view/files/__root__/views/__name__View/__name__View.js
@@ -0,0 +1,15 @@
+import React, { Component, PropTypes } from 'react';
+import css from './<%= pascalEntityName %>View.css';
+
+const propTypes = {
+};
+
+class <%= pascalEntityName %>View extends Component {
+  render() {
+    return (
+    );
+  }
+}
+
+<%= pascalEntityName %>View.propTypes = propTypes;
+export default <%= pascalEntityName %>View;
--- src/styles/css-global-vars.js
+++ src/styles/css-global-vars.js
@@ -1,4 +1,8 @@
+// These global variables will be available in all of the css using
+// a PostCSS plugin advanced-variables.  You can access them by prepending
+// a '$' for the variable name.  For example: body { background: $darkGray };
 module.exports = {
+  // Colors
   darkGray: '#1C2023',
   lightGray: '#7E899B',
 };
